4e7f4407ea9bdd6424550140c03c375c0a290ec1
alejandro gonzalez flores <agf_001@outlook.com>
2026-03-02 20:23:27 -0800

    Agregando la logica en Controllers.ts

--- src/controllers.ts
+++ src/controllers.ts
@@ -2,6 +2,28 @@ import { PelisCollection, Peli } from "./models";
 
 class PelisController {
   constructor(private model: PelisCollection) {}
+
+  get(options: { id?: number; search?: { title?: string; tag?: string } }): Promise<Peli[]> | Promise<Peli> {
+    if (options.id !== undefined) {
+      return this.model.getById(options.id);
+    } else if (options.search !== undefined) {
+      return this.model.search(options.search);
+    } else {
+      return this.model.getAll();
+    }
+  }
+getOne(options: { id?: number; search?: { title?: string; tag?: string } }): Promise<Peli | undefined> {
+  return this.get(options).then((result) => {
+    if (Array.isArray(result)) {
+      return result[0]; // Devuelve el primer elemento si es un array
+    }
+    return result; // Si ya es un solo objeto, lo devuelve directamente
+  });
+}
+
+add(peli: Peli): Promise<boolean> {
+  return this.model.add(peli);
+}
 }
 
 export { PelisController };
